d888dcf46ea9e0cfb2871cf68ef6723c766e4404
Thauweer <83612707+Thauweer@users.noreply.github.com>
2023-04-26 17:25:34 +0400

    Update store.ts

--- src/features/store.ts
+++ src/features/store.ts
@@ -2,18 +2,16 @@ import {configureStore} from '@reduxjs/toolkit'
 import { todoReducer } from './slices/todosSlice'
 import { appReducer } from './slices/appSlice'
 
-import { accountApi } from './api/account.api'
 import { todoApi } from './api/todo.api'
 
 export const store = configureStore({
     reducer: {
         todos : todoReducer,
         app: appReducer,
-        [accountApi.reducerPath]: accountApi.reducer,
         [todoApi.reducerPath]: todoApi.reducer
     },
     middleware: (getDefaultMiddleware) => 
-    getDefaultMiddleware().concat(accountApi.middleware, todoApi.middleware)
+    getDefaultMiddleware().concat(todoApi.middleware)
 })
 
 export type TypeRootState = ReturnType<typeof store.getState>
